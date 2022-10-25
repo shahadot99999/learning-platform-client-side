c5214e5e98eeab65a3a4111b03e58dac4bf7ca56
shahadot99999 <shahadot99999@gmail.com>
2022-10-25 10:01:40 +0600

    sidenavbar set up and connect server side

--- src/Page/Shared/SideNav/SideNav.js
+++ src/Page/Shared/SideNav/SideNav.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SideNav = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/educations-categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
         <div>
-            <h4>This is SideNav.</h4>
+            <h4>All Category: {categories.length} </h4>
+            <div>
+                {
+                    categories.map(category => <p key={category.id}><Link to={`/category/${category.id}`}>{category.name}</Link></p>)
+                }
+            </div>
         </div>
     );
 };

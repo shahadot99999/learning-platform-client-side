b8401adcc7745e97b6b1c9644b85fcaf62529f35
shahadot99999 <shahadot99999@gmail.com>
2022-10-25 17:10:33 +0600

    containt detail display show

--- src/Page/Shared/ContentDetails/ContentDetails.js
+++ src/Page/Shared/ContentDetails/ContentDetails.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 const ContentDetails = ({ educations }) => {
+    const { _id, title, image_url, details } = educations;
     return (
-        <div>
-            {educations.title}
-        </div>
+        <Card className=" ">
+
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Img varient='top' src={image_url} />
+                <Card.Text>
+                    {
+                        details.length > 200 ?
+                            <p>{details.slice(0, 250) + '...'}<Link to={`/educations/${_id}`}>Read More</Link></p>
+                            : <p>{details}</p>
+                    }
+                </Card.Text>
+
+            </Card.Body>
+
+        </Card>
     );
 };
 
